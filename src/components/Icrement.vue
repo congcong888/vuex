c36aab1219827cedbbd72c8376ddd4d64e511050
congcong888 <tiancong0808@163.com>
2017-10-12 15:11:55 +0800

    vuex应用

---
+++ src/components/Icrement.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+      <h2>计数器</h2>
+      <button @click="jian">-</button>
+      {{num}}
+      <button @click="add">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      // num:100
+      num: this.$store.state.num //引用vuex里定义的变量
+    }
+  },
+  methods:{
+    add(){
+      this.num++;
+    },
+    jian(){
+      this.num--;
+    }
+  }
+}
+</script>
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Icrement from '@/components/Icrement'
 
 Vue.use(Router)
 
@@ -8,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: HelloWorld
+      name: 'Icrement',
+      component: Icrement
     }
   ]
 })
